6d5172cc00ebe6a6a4d00079806c21962bf8a65c
Aimee Tonks <aimztonks@gmail.com>
2023-09-16 14:45:40 +0100

    contact

--- pages/contact.jsx
+++ pages/contact.jsx
@@ -35,19 +35,19 @@ export default function Contact(){
             <div>
                 <div className="social">
                 <h1 className={utilStyles.heading2Xl}>Contact Me!</h1>
-                    <div className="social" id="instagram">
+                    <div className ="social" id="instagram">
                         <Link href="https://www.instagram.com/ratinavan/">
                             <h1>Instagram</h1>
                             <p>@Ratinavan</p>
                         </Link>
                     </div>
-                    <div className="social" id="twitter">
+                    <div className ="social" id="twitter">
                         <Link href="https://twitter.com/ratinavan">
                             <h1>Twitter</h1>
                             <p>@Ratinavan</p>
                         </Link>
                     </div>
-                    <div className="social" id="discord">
+                    <div className ="social" id="discord">
                         <h1>Discord</h1>
                         <p>@Ratinavan</p>
                     </div>
