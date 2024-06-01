e4be836e87dc309ed0e9cda9ef681b2cb2f780a8
Aimee Tonks <aimztonks@gmail.com>
2024-06-01 15:14:51 +0100

    thank you page

--- pages/formbuild.js
+++ pages/formbuild.js
@@ -42,6 +42,7 @@ export default function FormStructure(){
                 setType('')
                 setNumber('')
                 setStyle('')
+                window.location.href = '/submitted'
             }
         })
     }
